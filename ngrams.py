8f5150775d0a9222bba8b87d7bbb1c9fa4d0c261
Eloy Geenjaar <eloyg@live.nl>
2019-03-06 16:43:32 +0100

    New n-gram function, just run python ngrams.py and it should work

--- ngrams.py
+++ ngrams.py
@@ -1,7 +1,8 @@
 import pandas as pd
-from nltk import ngrams
+import numpy as np
+import re
 
-#df = pd.DataFrame(pd.read_csv('total_data.csv',index_col=0,low_memory=False))
+df = pd.DataFrame(pd.read_csv('total_data.csv',index_col=0,low_memory=False))
 dtypes = {
     'AnonID': 'str',
     'Query': 'str',
@@ -10,28 +11,48 @@ dtypes = {
     'ClickUrl': 'str',
 }
 
+def ngrams(string, i):
+    string = string.replace('-',' ')
+    string = string.replace('.',' ')
+    string = re.sub(' +', ' ', string)
+    suffixes = []
+    prefixes = []
+    if string == ' ':
+        suffixes = [None]*i
+    else:
+        words = string.split(' ')
+        for n in range(1,i+1):
+            if len(words) == n:
+                suffix = str(string)
+                prefix = None
+            elif len(words) > n:
+                suffix = words[-n:]
+                prefix = str(words[0:(len(words)-n)])
+            else:
+                suffix = None
+                prefix = None
+            if isinstance(suffix, list):
+                suffix = ''.join(suffix)
+            suffixes.append(suffix)
+            prefixes.append(prefix)
+    return prefixes, suffixes
+
+
 # Test file, i only used the first 3000 lines or something like that
-fn = 'AOL-user-ct-collection/user-ct-test-collection-01.txt'
-df = pd.DataFrame(pd.read_csv(fn, sep="\t", dtype=dtypes))
+#fn = 'AOL-user-ct-collection/user-ct-test-collection-01.txt'
+#df = pd.DataFrame(pd.read_csv(fn, sep="\t", dtype=dtypes))
 j = 0
-grams = []
-# The paper uses n=1..6 for the n-grams
-for n in range(1,7):
-    # Loop through each query
-    for i in range(df['Query'].size):
-        # Generate n-grams based on the query and n
-        gram_gens = ngrams(str(df['Query'].iloc[i]).split(),n)
-        # Get the n-grams and append them to a list
-        for gram_gen in gram_gens:
-            grams.append(' '.join(gram_gen))
-    # Create pandas dataframe
+n = 6
+suffixes = []
+# Loop through each query
+for i in range(df['Query'].size):
+    # Generate n-grams based on the query and n
+    prefix, suffix = ngrams(str(df['Query'].iloc[i]),n)
     if j == 0:
-        df_grams = pd.DataFrame(grams, index = range(len(grams)), columns=[str(n)])
+        suffix_arr = np.asarray(suffix)
         j = 1
     else:
-        temp = pd.DataFrame(grams, index = range(len(grams)),columns=[str(n)])
-        df_grams = pd.concat([df_grams, temp], axis=1)
-    grams = []
+        suffix_arr = np.vstack((suffix_arr,np.asarray(suffix)))
 
-# Print the most frequent 1-grams
-print(df_grams['1'].value_counts())
+df_grams = pd.DataFrame(suffix_arr,columns = range(1,(n+1)))
+df_grams.to_csv('./total_grams_dataset.csv')
