0c40499bedd8b67d1d1f21c3f55139b3fff3e687
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2019-03-07 09:42:08 +0100

    Use 6 ngrams

--- ngrams.py
+++ ngrams.py
@@ -1,8 +1,21 @@
 import pandas as pd
 import numpy as np
 import re
+import os
+import os.path
 
-chunks = pd.read_csv('data.csv',index_col=0,low_memory=False, chunksize=100000)
+IN_FILE = 'total_data.csv'
+OUT_FILE = 'total_grams_dataset.csv'
+CHUNK_SIZE = 100000
+
+if os.path.exists(OUT_FILE): os.remove(OUT_FILE)
+
+with open(OUT_FILE, 'a') as the_file:
+    the_file.write('QueryTime,AnonID,Query,ItemRank,ClickURL,ngram1,ngram2,ngram3,ngram4,ngram5,ngram6\n')
+
+num_chunks = int(sum(1 for row in open(IN_FILE, 'r')) / CHUNK_SIZE) + 1
+
+chunks = pd.read_csv(IN_FILE,index_col=0,low_memory=False, chunksize=CHUNK_SIZE)
 dtypes = {
     'AnonID': 'str',
     'Query': 'str',
@@ -11,7 +24,7 @@ dtypes = {
     'ClickUrl': 'str',
 }
 
-NUMBER_OF_NGRAMS = 4
+NUMBER_OF_NGRAMS = 6
 
 def suffix_ngrams(string):
     words = re.sub(' +|-|\.', ' ', string).split()
@@ -22,11 +35,12 @@ def suffix_ngrams(string):
 
     return ngrams + [None]*(NUMBER_OF_NGRAMS - len(ngrams))
 
-chunk_id = 0
+chunk_id = 1
+
 
 for df in chunks:
-    print("Processing chunk " + str(chunk_id))
+    print("Processing chunk " + str(chunk_id) + " of " + str(num_chunks))
     chunk_id = chunk_id+1
     df = df.fillna('')
-    df["ngram1"], df["ngram2"], df["ngram3"], df["ngram4"] = zip(*df["Query"].map(suffix_ngrams))
-    df.to_csv('total_grams_dataset.csv', mode='a', header=False)
+    df["ngram1"], df["ngram2"], df["ngram3"], df["ngram4"], df["ngram5"], df["ngram6"] = zip(*df["Query"].map(suffix_ngrams))
+    df.to_csv(OUT_FILE, mode='a', header=False)
