0a0d52eddfde8ecc8b7529eabbad39292f576545
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2019-03-12 15:20:17 +0100

    Add additional features to featuregeneration

--- 6-Core-IR-FeatureGeneration.py
+++ 6-Core-IR-FeatureGeneration.py
@@ -25,9 +25,10 @@
 import pandas as pd
 import nltk
 
-TRAINING_FILE = 'popular_query.csv'
-FEATURES_FILE = 'features.csv'
+TRAINING_FILE = 'generated_candidate_head.csv'
+FEATURES_FILE = 'training_features.csv'
 SUFFIX_FILE = 'popular_suffix.csv'
+QUERY_FILE = 'popular_query.csv'
 
 CHUNK_SIZE = 10000
 
@@ -39,8 +40,7 @@ CHUNK_SIZE = 10000
 
 ngram_cols = ['ngramfreq_{}'.format(i) for i in range(1,7)]
 
-def ngram_apply(row, suffix_df):
-    query = row.Query
+def ngram_apply(query, suffix_df):
     words = query.split()
     ngrams = []
         
@@ -58,6 +58,12 @@ def ngram_apply(row, suffix_df):
     
     return pd.Series(ngrams)
 
+def query_freq(query, query_df):
+    try:
+        return int(query_df.at[query, 'counts'])
+    except KeyError:
+        return 0
+
 
 # In[ ]:
 
@@ -65,21 +71,22 @@ def ngram_apply(row, suffix_df):
 suffix_df = pd.read_csv(SUFFIX_FILE, index_col=0, dtype=object, low_memory=False)
 suffix_df = suffix_df.set_index('ngram')
 
+query_df = pd.read_csv(QUERY_FILE, index_col='Query', dtype=object, low_memory=False)
+
 
 # In[ ]:
 
 
 dtypes = {
-    'Index': 'int64',
-    'AnonID': 'str',
+    'Prefix': 'str',
     'Query': 'str',
-    'QueryTime': 'str',
+    'Suffix': 'str',
     'ItemRank': 'str',
     'ClickURL': 'str',
 }
 
 # only load index and Query
-chunks = pd.read_csv(TRAINING_FILE, index_col=0, dtype=dtypes, low_memory=False, chunksize=CHUNK_SIZE)
+chunks = pd.read_csv(TRAINING_FILE, dtype=dtypes, low_memory=False, chunksize=CHUNK_SIZE)
 
 # Count the number of chunks in this file
 num_chunks = int(sum(1 for row in open(TRAINING_FILE, 'r')) / CHUNK_SIZE) + 1
@@ -89,6 +96,8 @@ chunk_id = iter(range(1, num_chunks+1))
 # In[ ]:
 
 
+with open(FEATURES_FILE, 'w') as the_file: the_file.write(',Prefix,Query,Suffix,ngramfreq_1,ngramfreq_2,ngramfreq_3,ngramfreq_4,ngramfreq_5,ngramfreq_5,query_freq,prefix_len_c,prefix_len_w,suffix_len_c,suffix_len_w,len_c,len_w,end_space\n')
+
 for df in chunks:
     print("Processing chunk {} of {}".format(next(chunk_id), num_chunks), end="\r")
     
@@ -97,7 +106,16 @@ for df in chunks:
 
     df = df.reset_index(drop=True)
     
-    df[ngram_cols] = df.apply(ngram_apply, suffix_df=suffix_df, axis=1)
+    df[ngram_cols] = df.Query.apply(ngram_apply, suffix_df=suffix_df)
+    df['query_freq'] = df.Query.apply(query_freq, query_df=query_df)
+    df['prefix_len_c'] = df.Prefix.str.len()
+    df['prefix_len_w'] = df.Prefix.str.split().str.len()
+    df['suffix_len_c'] = df.Suffix.str.len()
+    df['suffix_len_w'] = df.Suffix.str.split().str.len()
+    df['len_c'] = df.Query.str.len()
+    df['len_w'] = df.Query.str.split().str.len()
+    df['end_space'] = df.Prefix.str.endswith(' ').astype(int)
+    
     
     df.to_csv(FEATURES_FILE, mode='a', header=False)
 
